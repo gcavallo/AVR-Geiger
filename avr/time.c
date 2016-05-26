1aae1a6fe8c6884a569cdfa72fd3ba217341edb4
Gabriel Cavallo <gabrielcavallo@mail.com>
2016-05-26 06:02:56 -0400

    Interrupt on TIMER1 instead of TIMER0

--- avr/time.c
+++ avr/time.c
@@ -19,13 +19,13 @@
 #include "time.h"
 
 void timer1_init(void) {
-	OCR1A = 0x3D08;                     // (clock / prescaler) * seconds) - 1
+	OCR1A = 0x3D08;                     // 16 MHz / 1024 prescaler * 1 sec - 1
 	TCCR1B |= _BV(WGM12);               // set TIMER1 to CTC mode
 	TIMSK1 |= _BV(OCIE1A);              // set interrupt on CTC match
 	TCCR1B |= _BV(CS12) | _BV(CS10);    // start TIMER1 with 1024 prescaler
 	seconds = 0;                        // reset TIMER1 to 0
 }
 
-ISR (TIMER0_COMPA_vect) {    // define TIMER1 interrupt
+ISR(TIMER1_COMPA_vect) {    // define TIMER1 interrupt
 	seconds++;
 }
