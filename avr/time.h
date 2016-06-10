6f5e8cf48a006e5029742d4249b6cc47b6e8ab89
Gabriel Cavallo <gabrielcavallo@mail.com>
2016-06-10 19:53:09 -0400

    Add include guard to time.h and uart.h

--- avr/time.h
+++ avr/time.h
@@ -5,6 +5,11 @@
  *    GPLv3 License https://gnu.org/licenses/gpl.txt
  */
 
+#ifndef TIME_H
+#define TIME_H
+
 volatile uint16_t seconds;
 
 void timer1_init(void);
+
+#endif /* TIME_H */
--- avr/uart.h
+++ avr/uart.h
@@ -5,8 +5,13 @@
  *    GPLv3 License https://gnu.org/licenses/gpl.txt
  */
 
+#ifndef UART_H
+#define UART_H
+
 void uart_init(void);
 char uart_getchar(void);
 void uart_putchar(char c);
 void uart_puts(char *str);
 char *itoa(int32_t);
+
+#endif /* UART_H */
