6e3c3d589525b15956da20f2d4eba61509d7ddc3
Gabriel Cavallo <gabrielcavallo@mail.com>
2016-06-11 19:15:50 -0400

    Set external interupt to falling edge on INT0

--- avr/main.c
+++ avr/main.c
@@ -19,13 +19,16 @@ ISR(INT0_vect) {
 }
 
 int main(void) {
-	uart_init();        // initialize USART
-	cli();              // disable global interrupts
-	timer1_init();      // initialize TIMER1
-	DDRD &= ~_BV(0);    // reset INT0 to input mode
-	PORTD |= _BV(0);    // set INT0 to high
-	sei();              // enable global interrupts
-	cpm = 0;            // reset cpm counter
+	cli();                   // disable global interrupts
+	uart_init();             // initialize USART
+	timer1_init();           // initialize TIMER1
+	DDRD &= ~_BV(DDD2);      // clear INT0 pin to input mode
+	PORTD |= _BV(PORTD2);    // set INT0 pin to high
+	EICRA |= _BV(ISC01);     // set INT0 to falling edge
+	EIMSK |= _BV(INT0);      // enable external interupt on INT0
+	cpm = 0;                 // reset cpm counter
+	seconds = 0;             // reset seconds counter
+	sei();                   // enable global interrupts
 
 	while(1) {
 		if (seconds >= 60) {
